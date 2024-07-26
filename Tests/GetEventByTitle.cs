32061439cc98480b0c7632db267be09c9271eafe
David Dieruf <david@dierufventures.com>
2024-07-26 07:18:18 -0400

    Added params to get event by title test

--- Tests/GetEventByTitle.cs
+++ Tests/GetEventByTitle.cs
@@ -1,4 +1,4 @@
-﻿using CronicleClient;
+using CronicleClient;
 using CronicleClient.Models;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
@@ -34,7 +34,7 @@ public class GetEventByTitle
             Title = "New title",
             Enabled = true,
             Category = "general",
-            Plugin = "testplug",
+            Plugin = "shellplug",
             Target = "allgrp",
             Timing = new Timing()
             {
@@ -43,14 +43,37 @@ public class GetEventByTitle
                 Days = [25],
                 Months = [8],
                 Years = [2024]
+            },
+            Parameters = new Dictionary<string, object>()
+            {
+                { "script", """
+                            echo 'Hello, World!'
+
+                            echo 'This is a test of the Cronicle Shell Plugin'
+                            """ },
+                { "annotate", true },
+                { "json", true }
             }
         };
         var eventId = await _cronicleClient.Event.Create(eventData: newEvent, cancellationToken: _cancellationToken);
         eventId.Should().NotBeEmpty();
 
         // Act
-        await FluentActions.Invoking(() => _cronicleClient.Event.GetByTitle(eventTitle: newEvent.Title, cancellationToken: _cancellationToken))
-            .Should().NotThrowAsync<KeyNotFoundException>();
+        var eventData = await _cronicleClient.Event.GetByTitle(eventTitle: newEvent.Title, cancellationToken: _cancellationToken);
+        
+        // Assert
+        eventData.Should().NotBeNull();
+        eventData!.Title.Should().Be(newEvent.Title);
+        eventData!.Plugin.Should().Be(newEvent.Plugin);
+        eventData!.Target.Should().Be(newEvent.Target);
+        eventData!.Timing.Should().BeEquivalentTo(newEvent.Timing);
+        eventData!.Category.Should().Be(newEvent.Category);
+        eventData!.Enabled.Should().BeTrue();
+        
+        // Convert all params to string to compare
+        eventData!.Parameters.Should().Contain(q => q.Key == "script" && q.Value.ToString() == newEvent.Parameters[q.Key].ToString());
+        eventData!.Parameters.Should().Contain(q => q.Key == "annotate" && q.Value.ToString() == newEvent.Parameters[q.Key].ToString());
+        eventData!.Parameters.Should().Contain(q => q.Key == "json" && q.Value.ToString() == newEvent.Parameters[q.Key].ToString());
 
         // Cleanup
         await _cronicleClient.Event.Delete(eventId: eventId, cancellationToken: _cancellationToken);

fe1770bb365d7fdb837cca28ba6b29cdc418885d
David Dieruf <david@dierufventures.com>
2024-07-27 08:55:52 -0400

    More comments

--- CronicleClient/CronicleClient.cs
+++ CronicleClient/CronicleClient.cs
@@ -4,6 +4,12 @@ using Microsoft.Extensions.Logging;
 
 namespace CronicleClient;
 
+/// <summary>
+/// Cronicle API client
+/// </summary>
+/// <param name="baseUrl"></param>
+/// <param name="apiToken"></param>
+/// <param name="logger"></param>
 public class Client(string baseUrl, string apiToken, ILogger logger)
 {
   private readonly HttpClient _apiClient = new()
@@ -11,8 +17,17 @@ public class Client(string baseUrl, string apiToken, ILogger logger)
     BaseAddress = new Uri(string.Concat(baseUrl.TrimEnd('/'), "/api/app/")),
     DefaultRequestHeaders = { { "X-API-Key", apiToken } }
   };
-
+  
+  /// <summary>
+  /// A Cronicle event
+  /// </summary>
   public CronicleEvent Event => new(_apiClient, logger);
+  /// <summary>
+  /// A Cronicle job
+  /// </summary>
   public CronicleJob Job => new(_apiClient, logger);
+  /// <summary>
+  /// A Cronicle master
+  /// </summary>
   public CronicleMaster Master => new(_apiClient, logger);
 }

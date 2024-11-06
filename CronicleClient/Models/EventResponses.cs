4af895cc06e97e0b7a3e0de0706fc39bdaa61094
alsetstreampay-admin <eyamada@alset.com.mx>
2024-11-06 00:37:23 -0700

    Similar to BaseResponse, except it also returns EventData

---
+++ CronicleClient/Models/EventResponses.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CronicleClient.Models
+{
+    internal record EventResponse
+    {
+        [JsonPropertyName("code")]
+        [JsonConverter(typeof(Common.IntToStringJsonConverter))]
+        public string Code { get; set; } = null!;
+
+        [Required]
+        [JsonPropertyName("eventData")]
+        [JsonConverter(typeof(EventData))]
+        public string Event { get; set; }
+
+    }
+        /*
+         * these ones might require special conditions
+        internal record AddResponse(object Code, string Id = default!);
+
+        internal record DeleteResponse(object Code, string Description);
+        internal record UpdateEventResponse(bool IsSucess, string Id = default!);
+        internal record UpdateResponse(int Code, string Id = default!);
+        internal record DisableResponse(object Code, string Description);
+        internal record ProcessResponse(string Description, string EventId = default!, string Code = Utility.CodeSuccess);
+    */
+}
